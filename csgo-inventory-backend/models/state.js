b86593d3cf1df350e8cfa8df016a2f77de8a5b4d
Du Duong <khaidu2002@gmail.com>
2023-12-24 21:29:11 -0500

    state model

---
+++ csgo-inventory-backend/models/state.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+import Item from './item.js';
+
+const pistolNumber = 4
+const midTierNumber = 5
+const highTierNumber = 5
+
+const LoadoutStateSchema = new mongoose.Schema({
+    startingPistol: {
+        type: Item.schema,
+        required: true
+    },
+    pistols: {
+        type: [Item.schema],
+        required: true,
+        // validation for exactly 4 pistols
+        validate: (pistols) => pistols.length === pistolNumber
+    },
+    midTier: {
+        type: [Item.schema],
+        required: true,
+        // validation for exactly 5 mid tier weapons
+        validate: (midTier) => midTier.length === midTierNumber
+    },
+    highTier: {
+        type: [Item.schema],
+        required: true,
+        // validation for exactly 5 high tier weapons
+        validate: (highTier) => highTier.length === highTierNumber
+    },
+    knife: {
+        type: Item.schema,
+        required: true
+    },
+    gloves: {
+        type: Item.schema,
+        required: true
+    },
+    agent: {
+        type: Item.schema,
+        required: true
+    }
+});
+
+const builderStateSchema = new mongoose.Schema({
+    tSide: {
+        type: LoadoutStateSchema,
+        required: true
+    },
+    ctSide: {
+        type: LoadoutStateSchema,
+        required: true
+    }
+})
+
+const BuilderState = mongoose.model('BuilderState', builderStateSchema);
+
+export default BuilderState;
