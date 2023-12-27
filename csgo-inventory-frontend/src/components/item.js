a259c9e6773d2cb69df028cf72334f8ba3104798
Du Duong <khaidu2002@gmail.com>
2023-12-27 02:57:43 -0500

    with 4 screens

--- csgo-inventory-frontend/src/components/item.js
+++ csgo-inventory-frontend/src/components/item.js
@@ -32,7 +32,7 @@ export default function Item(props) {
         <div className={`col-span-1 border rounded-lg shadow bg-gray-800 border-gray-700 border-b-4 mb-3 flex flex-col items-center hover:bg-gray-700 cursor-pointer transition-all duration-300 ease-in-out transform hover:scale-105`} onClick={props.onClick}>
             {/* Image container with flex properties to center the image */}
             <div className="flex justify-center items-center self-stretch">
-                <img className="rounded-t-lg object-fit max-h-40" src={`https://steamcommunity-a.akamaihd.net/economy/image/${props.item.icon_url}/${Math.max(parseInt(windowSize.width/12), 100)}x${Math.max(parseInt(windowSize.height/5), 100)}`} alt={props.item.name} />
+                <img className="rounded-t-lg object-fit max-h-40" src={`https://steamcommunity-a.akamaihd.net/economy/image/${props.item.icon_url}/${Math.max(parseInt(windowSize.width/12), 70)}x${Math.max(parseInt(windowSize.height/10), 70)}`} alt={props.item.name} />
             </div>
             {/* Text container for the name */}
             <div className="w-full text-center p-2">
